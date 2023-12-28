af403e51e6022a2813c4433dc7ffc48741831c5e
Pravin Singh <pravin130204@gmail.com>
2023-12-28 21:17:32 +0530

    Production Grade React Components

--- AppwriteMegaBlog/src/appwrite/config.js
+++ AppwriteMegaBlog/src/appwrite/config.js
@@ -1,4 +1,4 @@
-import conf from '../conf.js'
+import conf from '../conf/conf.js'
 import { Client, ID, Databases, Storage, Query } from 'appwrite'
 //bucket services means--> storage services 
 export class Service{   //storage ke liye Services banayi hai
