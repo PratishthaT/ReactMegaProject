861cbdf30ab283bb5b2a6767928276d8c9fc47f2
Pravin Singh <pravin130204@gmail.com>
2024-01-03 15:36:18 +0530

    Debugging

--- AppwriteMegaBlog/src/appwrite/config.js
+++ AppwriteMegaBlog/src/appwrite/config.js
@@ -1,27 +1,26 @@
-import conf from '../conf/conf.js'
-import { Client, ID, Databases, Storage, Query } from 'appwrite'
-//bucket services means--> storage services 
-export class Service{   //storage ke liye Services banayi hai
+import conf from '../conf/conf.js';
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
     client = new Client();
     databases;
-    bucket;  //storage
+    bucket;
 
-    constructor(){  //account tab banna chahiye jab constructor call ho
-        this.client                                       // from documentation
-            .setEndpoint(conf.appwriteUrl)                // from documentation
-            .setProject(conf.appwriteProjectId);          // from documentation
-            this.databases = new Databases(this.client);  // from documentation
-            this.bucket = new Storage(this.client)        // from documentation
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
     }
-                    //YE SAB VALUES PASS KARNA HAI JAB BHI YE METHOD USE KARENGE
-    async createPost({title, slug, content, featuredImage, status, userId}) {
-        try{
-            return await this.databases.createDocument(             // from documentation
-                conf.appwriteDatabaseId, //[Database_ID]           
-                conf.appwriteCollectionId, //[Collection_ID]      
-                slug, // [Document_ID]
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
                 {
-                    //jo create karna tha unhe {} mai diya
                     title,
                     content,
                     featuredImage,
@@ -29,111 +28,110 @@ export class Service{   //storage ke liye Services banayi hai
                     userId,
                 }
             )
-        } catch(error){
-            console.log("Appwrite service :: createPost :: error", error); //COMMON AMONG ALL METHODS
+        } catch (error) {
+            console.log("Appwrite serive :: createPost :: error", error);
         }
-
     }
 
-    async updatePost(slug, {title,  content, featuredImage, status}){
+    async updatePost(slug, { title, content, featuredImage, status }) {
         try {
             return await this.databases.updateDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
-                slug,  // [Document_ID]
-                { //jo update karna hai vo diya in {} mai
+                slug,
+                {
                     title,
                     content,
                     featuredImage,
                     status,
+
                 }
             )
         } catch (error) {
-            console.log("Appwrite service :: updatePost :: error",error);
+            console.log("Appwrite serive :: updatePost :: error", error);
         }
     }
 
-    async deletePost(slug){
+    async deletePost(slug) {
         try {
             await this.databases.deleteDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
-                slug //[Document_ID]
+                slug
+
             )
-            return true //ha bhai ye delete hogaya hai ye batane ke liye
-            
+            return true
         } catch (error) {
-            console.log("Appwrite service :: deletePost :: error", error)
+            console.log("Appwrite serive :: deletePost :: error", error);
             return false
         }
     }
 
-    async getPost(slug){
+    async getPost(slug) {
         try {
             return await this.databases.getDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug
+
             )
-            
         } catch (error) {
-            console.log("Appwrite service :: getPost :: error",error)
+            console.log("Appwrite serive :: getPost :: error", error);
             return false
         }
     }
 
-    async getPosts(queries = [Query.equal("status","active")]){
-         try {
+    async getPosts(queries = [Query.equal("status", "active")]) {
+        try {
             return await this.databases.listDocuments(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 queries,
-            )
-         } catch (error) {
-            console.log("Appwrite service :: getPosts :: error", error);
-            return false //ek bhi value nhi mil rhi ho to false return kardenge
-         }
-    }
 
 
-    //file upload service
-    async uploadFile(file){
-        try {
-            return await this.bucket.createFile(
-                conf.appwriteBucketId ,
-                ID.unique(),
-                file 
             )
-            
         } catch (error) {
-            console.log("Appwrite service :: uploadFile :: error", error);
+            console.log("Appwrite serive :: getPosts :: error", error);
             return false
         }
     }
 
-    async deleteFile(fileId){
+    // file upload service
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false
+        }
+    }
+
+    async deleteFile(fileId) {
         try {
             await this.bucket.deleteFile(
                 conf.appwriteBucketId,
                 fileId
             )
             return true
-            
         } catch (error) {
-            console.log("Appwrite service :: deleteFile :: error", error);
+            console.log("Appwrite serive :: deleteFile :: error", error);
             return false
         }
     }
 
-     getFilePreview(fileId){ //promise etc nhi hai to instantly chal jata hai 
+    getFilePreview(fileId) {
         return this.bucket.getFilePreview(
             conf.appwriteBucketId,
             fileId
         )
-     }
+    }
 }
 
 
-const service = new Service(); 
-export default service;
-//object banake export kardiya to external files mai jab object ko use karenge tab hi method banke use hoga 
+const service = new Service()
+export default service
--- AppwriteMegaBlog/src/components/AuthLayout.jsx
+++ AppwriteMegaBlog/src/components/AuthLayout.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import { useEffect,useState } from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
 
+  useEffect(() => {
+    //TODO: make it more easy to understand
 
-export default function Protected({children, authentication=true}) {
+    // if (authStatus ===true){
+    //     navigate("/")
+    // } else if (authStatus === false) {
+    //     navigate("/login")
+    // }
 
-    const navigate = useNavigate()
-    const [loader, setLoader] = useState(true)
-    const authStatus = useSelector(state=> state.auth.status)
+    //let authValue = authStatus === true ? true : false
 
-    useEffect(()=>{
-        if(authentication && authStatus!== authentication){
-            navigate("/login")
-        }else if(!authentication && authStatus!== authentication){
-            navigate("/")
-        }
-        setLoader(false)
-    },[authStatus, navigate,authentication])
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
 
-  return loader ? <h1>Loading...</h1>:<>{children}</>
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
 }
