faa9d5e67c44341f7c936c154e774e17b7de8b01
Pravin Singh <pravin130204@gmail.com>
2024-03-30 11:33:56 +0530

    Ghost post problem solved

--- AppwriteMegaBlog/src/pages/AllPosts.jsx
+++ AppwriteMegaBlog/src/pages/AllPosts.jsx
@@ -7,20 +7,28 @@ function AllPosts() {
   const userData = useSelector((state) => state.auth.userData);
   const [posts, setPosts] = useState([]);
   useEffect(() => {}, []);
-  appwriteService.getPosts([]).then((posts) => {
-    if (posts) {
-      setPosts(posts.documents);
-    }
-  });
+  try {
+    appwriteService.getPosts([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
     <div className="w-full py-8 bg-white">
       <Container>
         <div className="grid grid-cols-1 md:flex md:flex-wrap">
-          {posts.map((post) => (
-            <div key={post.$id} className="p-1 w-full md:p-2 md:w-1/4">
-              {post.status === "inactive" ? post?.userId === userData?.$id && <PostCard {...post} /> : <PostCard {...post} />}
-            </div>
-          ))}
+          {posts.map(
+            (post) =>
+              (post.status !== "inactive" || post.userId === userData?.$id) && (
+                <div key={post.$id} className="p-1 w-full md:p-2 md:w-1/4">
+                  <PostCard {...post} />
+                </div>
+              )
+          )}
         </div>
       </Container>
     </div>
